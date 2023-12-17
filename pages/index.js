8b83707d9c30b0d274f3612340e6a2e08d404708
neerajram30 <neerajramachandran30@gmail.com>
2023-12-17 21:48:07 +0530

    init

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Home_6 from "./home/home_6";
+import Wrapper from "./layout/wrapper";
+
+const MainRoot = () => {
+  return (
+    <Wrapper>
+      <Home_6 />
+    </Wrapper>
+  );
+};
+
+export default MainRoot;
